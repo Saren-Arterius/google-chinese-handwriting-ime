6e11f530f3db37cbfd761c7cd06e2fc5e7f4a5e5
Saren Arterius <saren@wtako.net>
2018-01-22 01:26:36 +0800

    first

--- renderer.js
+++ renderer.js
@@ -1,3 +1,65 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+/* globals document, $, setInterval, ctr */
+
+const UI_POLL_INTERVAL_MS = 50;
+const {exec, execFile} = require('mz/child_process');
+
+let thisWindowID;
+let lastWindowID;
+let windowWidth;
+
+const initUI = () => {
+  $('body').css({overflow: 'hidden'});
+  windowWidth = $('body').width();
+  ctr.Ea.b.gh(); // Toggle handwrite
+  ctr.Ea.b.C.A.C[2].C.view.Ui(); // Toggle full size
+  $('.ita-hwt-grip').remove();
+  $('.ita-hwt-close').remove();
+};
+
+const getNumberOutput = async (cmd) => {
+  return parseInt((await exec(cmd))[0].trim(), 10);
+};
+
+const focusLastWindow = async () => {
+  if (!lastWindowID) {
+    throw new Error('Last window is empty');
+  }
+  await exec(`xdotool windowfocus ${lastWindowID}`);
+};
+
+const main = async () => {
+  initUI();
+  thisWindowID = await getNumberOutput('xdotool search "Google Chinese Handwriting IME"');
+  $('.ita-hwt-backspace').click(async () => {
+    await focusLastWindow();
+    await exec('xdotool key BackSpace');
+  });
+  setInterval(async () => {
+    let windowID = await getNumberOutput('xdotool getactivewindow');
+    if (windowID !== thisWindowID) {
+      lastWindowID = windowID;
+    }
+    let val = $('#source').val();
+    if (val.length > 0) {
+      $('#source').val('');
+      await focusLastWindow();
+      await execFile('xdotool', ['type', val]);
+    }
+    let newWidth = $('body').width();
+    if (windowWidth !== newWidth) {
+      ctr.Ea.b.C.A.C[2].C.view.Ui();
+      ctr.Ea.b.C.A.C[2].C.view.Ui();
+      windowWidth = newWidth;
+    }
+  }, UI_POLL_INTERVAL_MS);
+};
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  let script = document.createElement('script');
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+  script.onreadystatechange = main;
+  script.onload = script.onreadystatechange;
+  document.body.appendChild(script);
+});
+
